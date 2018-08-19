84d5d6959830b839063c7313a3a41f488c368193
adis.civgin <adis.civgin@upandrunningsoftware.com>
2018-08-19 23:07:54 +0200

    added event addPost-not dummy data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My First App';
+  storedPosts = [];
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
@@ -7,9 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = '';
+  enteredContent = '';
+  enteredTitle = '';
+
+  // Event moze biti slusan izvan komponente (izvozimo event)
+  @Output()postCreated = new EventEmitter();
+
   savePost() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
- }
+}
