e0f8708a149c0388abde2adecdc4b35ae142ef58
adis.civgin <adis.civgin@upandrunningsoftware.com>
2018-08-26 17:53:05 +0200

    conected node and angular I part

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,32 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+// var originsWhitelist = [
+//   'http://localhost:4200' //this is my front-end url for development
+// ];
+// var corsOptions = {
+//   origin: function (origin, callback) {
+//     var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+//     callback(null, isWhitelisted);
+//   },
+//   credentials: true
+// };
+// //here is the magic
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type', 'Access');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [{
@@ -23,5 +50,6 @@ app.use('/api/posts', (req, res, next) => {
     posts: posts
   });
 });
+
 
 module.exports = app;
--- src/app/posts/posts-service.ts
+++ src/app/posts/posts-service.ts
@@ -1,25 +1,35 @@
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 // Only one instance in whole app, not adding service in module.ts
 @Injectable({ providedIn: 'root' })
 export class PostsService {
-    private posts: Post[] = [];
-    private postUpdated = new Subject<Post[]>();
+  private posts: Post[] = [];
+  private postUpdated = new Subject<Post[]>();
 
-    getPosts() {
-        // Deep copy, not just address
-        return [...this.posts];
-    }
+  constructor(private httpClient: HttpClient) {
 
-    getPostUpdatedListener() {
-      return this.postUpdated.asObservable();
-    }
+  }
 
-    addPost(title: string, content: string) {
-        const post: Post = { title: title, content: content };
-        this.posts.push(post);
-        this.postUpdated.next([...this.posts]);
-    }
+  getPosts() {
+    this.httpClient.get<{ message: 'string', posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+          this.posts = postData.posts;
+          this.postUpdated.next([...this.posts]);
+      });
+    // Deep copy, not just address
+    return [...this.posts];
+  }
+
+  getPostUpdatedListener() {
+    return this.postUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.posts.push(post);
+    this.postUpdated.next([...this.posts]);
+  }
 }
