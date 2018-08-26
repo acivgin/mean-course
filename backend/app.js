ce9b386d96570296b8823213458328f56f2ff8fa
adis.civgin <adis.civgin@upandrunningsoftware.com>
2018-08-26 17:16:56 +0200

    added json dummy data and send on browser with status 200

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from express !!");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [{
+      id: "29228922",
+      title: "First Title",
+      content: "First Content"
+    },
+    {
+      id: "0938833d32",
+      title: "Second Title",
+      content: "Second Content"
+    },
+    {
+      id: "00000393",
+      title: "Third Title",
+      content: "Third Content"
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfuly !",
+    posts: posts
+  });
 });
 
 module.exports = app;
